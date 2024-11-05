136d56b8b6d7888549cd9f50bdc191ab814ca07a
omarsayed202 <omarsayedabdelmohasen@gmail.com>
2024-11-05 16:39:30 +0200

    update Test

--- src/test/java/com/Bills/Bills/BillsApplicationTests.java
+++ src/test/java/com/Bills/Bills/BillsApplicationTests.java
@@ -3,15 +3,12 @@ package com.Bills.Bills;
 import com.Bills.Bills.Items.Item;
 import com.Bills.Bills.Items.ItemRepository;
 import com.Bills.Bills.Items.ItemServices;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.Optional;
 
@@ -54,6 +51,20 @@ class BillsApplicationTests {
 		when(itemRepository.findById(id)).thenReturn(expected);
 		// Act
 		Optional<Item> getItem =itemServices.getbyid(4L);
+		System.out.println(getItem.get());
+		// Assert
+		assertEquals(expected, getItem);
+	}
+	@Test
+	void testgetItemById2() {
+		// Arrange
+		long id=5L;
+		Optional<Item> expected= Optional.of(new Item(5L,null,99,100,0,0,8900,LocalDate.of(2024, 9, 29),"omar"));
+		// Mocking repository response
+		System.out.println(expected.get());
+		when(itemRepository.findById(id)).thenReturn(expected);
+		// Act
+		Optional<Item> getItem =itemServices.getbyid(5L);
 		System.out.println(getItem.get());
 		// Assert
 		assertEquals(expected, getItem);
